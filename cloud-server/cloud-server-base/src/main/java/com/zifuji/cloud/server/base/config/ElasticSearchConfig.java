ec2f8c1f4bbbbdf88886dc28d0e0e82f2fefd896
tongcheng <983123596@qq.com>
2023-12-03 15:31:14 +0800

    1

---
+++ cloud-server/cloud-server-base/src/main/java/com/zifuji/cloud/server/base/config/ElasticSearchConfig.java
@@ -0,0 +1,11 @@
+package com.zifuji.cloud.server.base.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class ElasticSearchConfig {
+
+
+}
