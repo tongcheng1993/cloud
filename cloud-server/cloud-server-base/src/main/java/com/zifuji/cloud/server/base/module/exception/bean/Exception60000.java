42f422f5b1db6c06cd6a0b6b8c9e5266104dde51
tongcheng <1637706385@qq.com>
2024-07-15 20:05:25 +0800

    做一些修改

---
+++ cloud-server/cloud-server-base/src/main/java/com/zifuji/cloud/server/base/module/exception/bean/Exception60000.java
@@ -0,0 +1,15 @@
+package com.zifuji.cloud.server.base.module.exception.bean;
+
+public class Exception60000 extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	// 业务校验异常
+	public Exception60000(String string) {
+		super(string);
+	}
+
+}
